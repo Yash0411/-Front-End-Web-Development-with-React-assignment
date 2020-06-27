6bf5a0ca342ec1bc97dbe01fd3cced686a13d079
Yash0411 <59476582+Yash0411@users.noreply.github.com>
2020-06-27 16:48:42 +0530

    Add files via upload

---
+++ src/components/dishdetail.js
@@ -0,0 +1,215 @@
+import React, { useState } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+//render method for displaying the details and image of the dish
+function RenderDish({dish}){
+    if (dish != null)
+        return(
+            <Card>
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    else
+        return(
+            <div></div>
+        );
+}
+
+//render method for mapping the comments of the dish
+//the header is being used, as well as two lines for the comment and author/date
+function RenderComments({comments, postComment, dishId}) {
+    if(comments != null){
+        return(
+            <>
+            <div className="list-unstyled">                
+                <h4>Comments</h4><hr></hr>
+                {comments.map((comment) => {
+                    return(
+                        <div key={comment.id}>
+                            <div className="comment">{comment.comment}</div><br></br>
+                            <div className="author_date">{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</div><hr></hr>
+                        </div>
+                    )
+                })}
+            </div>
+            {/*We insert the CommentForm in the RenderComments to show the Modal with the Form. Toggled by a button.*/}
+            <CommentForm dishId={dishId} postComment={postComment} />
+            </>
+        )
+    } else {
+        return(
+            <div></div>
+        )
+    }
+}
+
+
+//CommentForm that displays the Modal for submitting a comment
+const CommentForm = (props) => {
+    
+    
+    //The various validators used for the author controller
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
+
+    //The isModalOpen used to toggle the Modal. Here, it is set to false
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    //The function that handles the submit for the form. Currently it doesnt do much.
+    const handleSubmit = (values) => {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        props.postComment(props.dishId, values.rating, values.author, values.comment);
+    }
+
+    //The function that toggles the modal. For some reason, if not done this way
+    //the whole thing doesnt work. I thought it could be done with the hook but 
+    //it doesn't so who knows.
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    }
+
+    //Here is the whole thing. The button, the modal and the form.
+    return(
+    <>
+        {/*Button that toggles the modal. */}
+        <Button outline onClick={toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+
+        {/*The modal that shows the form. */}
+        <Modal isOpen={isModalOpen} toggle={toggleModal}>
+            <ModalHeader toggle={toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+
+                {/*The form that was requested. It uses the react-redux-form */}
+                <LocalForm onSubmit={(values) => handleSubmit(values)}>
+
+                    {/*The rating controller. It uses a select control, with options from 1 to 5. */}
+                    <Row className="form-group">
+                        <Col md={10}>
+                            <Label htmlFor="rating" md={2}>Rating</Label>
+                            <Control.select model=".rating" name="rating">
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </Control.select>
+                        </Col>
+                    </Row>
+
+                    {/*The author text control which is verified by the validators at the top. Minimum len of 3, max len of 15 and it is required */}
+                    <Row className="form-group">
+                        <Col md={10}>
+                            <Label htmlFor="author" md={2}>Your Name</Label>
+                            <Control.text model=".author" id="author" name="author"
+                                placeholder="Author name"
+                                className="form-control"
+                                validators={{
+                                    required, minLength: minLength(3), maxLength: maxLength(15)
+                                }}
+                                />     
+                                
+                                {/*Show errors for the invalid inputs.*/}
+                                <Errors
+                                    className="text-danger" 
+                                    model=".author"
+                                    show="touched"
+                                    messages={{
+                                        required: 'Required, ',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength: 'Must be 15 characters or less'
+                                    }}
+                                >   
+                                </Errors>                           
+                        </Col>
+                    </Row>
+
+                    {/*The comment control, which is a text area of 6 rows. Nothing more to add. */}
+                    <Row className="form-group">                            
+                        <Col md={10}>
+                            <Label htmlFor="message" md={2}>Comment</Label>
+                            <Control.textarea model=".comment" id="comment" name="comment"
+                                rows="6"
+                                className="form-control" />
+                        </Col>
+                    </Row>
+
+                    {/*Submit button for the form.*/}
+                    <Row className="form-group">                            
+                        <Col md={{size: 10, offset: 2}}>
+                            <Button type="submit" color="primary">
+                                Submit
+                            </Button>
+                        </Col>
+                    </Row>
+                </LocalForm>
+            </ModalBody>
+        </Modal>
+    </>
+    )
+}
+
+
+//functional component
+const DishDetail = (props) => {
+
+    //what is being rendered by the component
+    //notice that the bootstrap columns are set in the className
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) 
+        return(
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                    <RenderComments comments={props.comments}
+                        postComment={props.postComment}
+                        dishId={props.dish.id}
+                    />
+                    </div>
+                </div>
+                </div>
+        )
+}
+
+export default DishDetail;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"></link>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css" rel = "stylesheet"></link>
+    <App />
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
